fb6f38146df00b9b2208d2d0223a24dfc7f63f6b
Kayexie <kaynion@hotmail.com>
2024-09-20 10:59:24 -0400

    update

--- user_auth/models.py
+++ user_auth/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False, db_index = True)
     email = models.EmailField(unique = True)
     google_id = models.CharField(unique = True, null = True, blank = True)
-    password = models.CharField(null = False, blank = False)
+    password = models.CharField()
     full_name = models.CharField(max_length = 255)
     age = models.IntegerField()
     address = models.CharField(max_length = 255)
